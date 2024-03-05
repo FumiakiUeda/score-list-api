0bce5ab42da112d5756aca60906125647b2b3d65
UmiKazeYu <uosius21@gmail.com>
2024-03-06 00:32:46 +0900

    add: Laravel Breezeをインストール

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-// scoresのCRUDのルーティング
-Route::get('/scores', 'App\Http\Controllers\ScoreController@index');
-Route::post('/score', 'App\Http\Controllers\ScoreController@store');
-Route::get('/score/{score:id}', 'App\Http\Controllers\ScoreController@edit');
-Route::patch('/score/{score:id}', 'App\Http\Controllers\ScoreController@update');
-Route::delete('/score/{score:id}', 'App\Http\Controllers\ScoreController@delete');
